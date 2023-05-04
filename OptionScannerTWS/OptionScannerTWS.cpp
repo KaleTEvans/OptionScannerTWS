769d1bb9fae11a272ee6ee1fd6202d09e7b57d89
Kale Evans <kaletevans@gmail.com>
2023-05-04 15:27:39 -0500

    Add project files.

---
+++ OptionScannerTWS/OptionScannerTWS.cpp
@@ -0,0 +1,58 @@
+/*========================================================================
+ * TwsApiC++ - Demonstration program
+ * Retrieving historical data from Interactive Brokers
+ * See the ///blocks for some explanation
+ *========================================================================
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+#include "tWrapper.h"
+
+
+//========================================================================
+// main entry
+//========================================================================
+int main(void) {
+    ///Easier: just allocate your wrapper and instantiate the EClientL0 with it.
+    tWrapper  YW(false);                      // false: not using the EReader
+    EClientL0* EC = EClientL0::New(&YW);
+
+    ///Easier: All structures defined by IB are immediately available.
+    ///Faster: Use of TwsApiDefs.h codes check typos at compile time
+    Contract          C;
+    C.symbol = "MSFT";
+    C.secType = *SecType::STK;              // instead of "STK"
+    C.currency = "USD";
+    C.exchange = *Exchange::IB_SMART;        // instead of "SMART"
+    //C.primaryExchange = *Exchange::AMEX;            // instead of "AMEX"
+
+    ///Easier: Use of TwsApiDefs.h makes code self explanatory,
+    ///        i.e. UseRTH::OnlyRegularTradingData instead of true or 1.
+    if (EC->eConnect("127.0.0.1", 7496, 0)) {
+        std::cout << "Connected to TWS Server" << std::endl;
+        EC->reqHistoricalData
+        (20
+            , C
+            , EndDateTime(2023, 05, 05)                   // 20130220 00:00:00
+            , DurationStr(1, *DurationHorizon::Days)  // instead of "1 M"
+            , *BarSizeSetting::_10_mins                  // instead of "1 day"
+            , *WhatToShow::TRADES                       // instead of "TRADES"
+            , UseRTH::OnlyRegularTradingData            // instead of 1
+            , FormatDate::AsDate                        // instead of 1
+        );
+
+        ///Easier: Call checkMessages() in a loop. No need to wait between two calls.
+        while (YW.notDone()) {
+            EC->checkMessages();
+        }
+
+    }
+
+    EC->eDisconnect();
+    delete EC;
+
+    return 0;
+}
---
+++ OptionScannerTWS/tWrapper.h
@@ -0,0 +1,73 @@
+/*====================================
+* EWrapper Class
+* This will receive callbacks from the TWS Api and translate to usable data
+* Can overwrite virtual functions to define your own output
+* The TwsApiC++ library uses the same functions and object names as the actual TWS Api
+*=====================================
+*/
+
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+///Easier: Just one include statement for all functionality
+#include "TwsApiL0.h"
+
+///Faster: Check spelling of parameter at compile time instead of runtime.
+#include "TwsApiDefs.h"
+using namespace TwsApi; // for TwsApiDefs.h
+
+
+// We can define our own eWrapper to implement only the functionality that we need to use
+class tWrapper : public EWrapperL0 {
+
+public:
+    bool m_Done, m_ErrorForRequest;
+    bool notDone(void) { return !(m_Done || m_ErrorForRequest); }
+
+    ///Easier: The EReader calls all methods automatically(optional)
+    tWrapper(bool runEReader = true) : EWrapperL0(runEReader) {
+        m_Done = false;
+        m_ErrorForRequest = false;
+    }
+
+    ///Methods winError & error print the errors reported by IB TWS
+    virtual void winError(const IBString& str, int lastError) {
+        fprintf(stderr, "WinError: %d = %s\n", lastError, (const char*)str);
+        m_ErrorForRequest = true;
+    }
+
+    virtual void error(const int id, const int errorCode, const IBString errorString) {
+        fprintf(stderr, "Error for id=%d: %d = %s\n"
+            , id, errorCode, (const char*)errorString);
+        m_ErrorForRequest = (id > 0);    // id == -1 are 'system' messages, not for user requests
+    }
+
+    virtual void connectionClosed() {
+
+    }
+
+    ///Safer: uncatched exceptions are catched before they reach the IB library code.
+    ///       The Id is tickerId, orderId, or reqId, or -1 when no id known
+    virtual void OnCatch(const char* MethodName, const long Id) {
+        fprintf(stderr, "*** Catch in EWrapper::%s( Id=%ld, ...) \n", MethodName, Id);
+    }
+
+    ///Faster: Implement only the method for the task.
+    /// => TwsApiC++  provides an empty implementation for each EWrapper method.
+    virtual void historicalData(TickerId reqId, const IBString& date
+        , double open, double high, double low, double close
+        , int volume, int barCount, double WAP, int hasGaps) {
+
+        ///Easier: EWrapperL0 provides an extra method to check all data was retrieved
+        if (IsEndOfHistoricalData(date)) {
+            m_Done = true;
+            return;
+        }
+
+        fprintf(stdout, "%10s, %5.3f, %5.3f, %5.3f, %5.3f, %7d\n"
+            , (const  char*)date, open, high, low, close, volume);
+    }
+};
